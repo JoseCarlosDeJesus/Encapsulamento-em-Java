993d581238bb18b86e006cbfdf66740da4b2be7f
José Carlos de Jesus Santana Júnior <73854111+JoseCarlosDeJesus@users.noreply.github.com>
2021-11-10 11:41:07 -0300

    Add files via upload

---
+++ PessoaFisica.java
@@ -0,0 +1,24 @@
+package br.com.pc2.semana6.pct1;
+
+import java.time.LocalDate;
+
+import br.com.pc2.semana6.pct2.PessoaJuridica;
+
+public class PessoaFisica extends Cliente {
+	private String cpf;
+	protected String rg;
+	public Character sexo;
+	LocalDate dataDeNascimento;
+
+	public String validarCpf() {
+		return "CPF validado";
+	}
+
+	public void testarVisibilidade() { /*aqui temos o polimorfismo(sobrescrita) de método: a classe Cliente já possui esse método
+mas aqui a implementamos de forma diferente,*/
+		PessoaFisica pf = new PessoaFisica();
+		PessoaJuridica pj = new PessoaJuridica();
+		Cliente c = new Cliente();
+	}
+
+}
